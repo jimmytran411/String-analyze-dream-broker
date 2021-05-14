ed98c02465e64806c54d726386b3d905f3332b71
An Tran <jimmytran411@gmail.com>
2021-05-14 15:15:24 +0300

    add react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { StringAnalyzer } from './Views/StringAnalyzer';
 
 const App: React.FC = () => {
   return (
-    <Grid container direction="column" spacing={2}>
-      <Grid item xs>
-        <h3 style={{ textAlign: 'center' }}>String Analyzer</h3>
-      </Grid>
-      <Grid item xs>
-        <StringAnalyzer />
-      </Grid>
-    </Grid>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={StringAnalyzer} />
+      </Switch>
+    </Router>
   );
 };
 
